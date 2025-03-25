9b5c68c5d9efb1f744e430848d02fd1a66249f67
Nick van Urk <nickyvanurk@gmx.com>
2025-03-25 15:54:55 +0900

    Simulate gravity on the server

--- server/src/unit.ts
+++ server/src/unit.ts
@@ -1,6 +1,9 @@
 import { Entity } from './entity';
 
 export class Unit extends Entity {
+  vy = 0;
+  gravity = -9.81;
+  floorHeight = 0;
   health = { current: 100, max: 100, min: 0 };
 
   constructor(
@@ -8,6 +11,16 @@ export class Unit extends Entity {
     public color: number,
   ) {
     super();
+  }
+
+  update(dt: number): void {
+    this.vy += (this.gravity * dt) / 1000;
+    this.y += (this.vy * dt) / 1000;
+
+    if (this.y < this.floorHeight) {
+      this.y = this.floorHeight;
+      this.vy = 0;
+    }
   }
 
   damage(amount: number) {
